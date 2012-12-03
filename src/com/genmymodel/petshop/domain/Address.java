c562ce3cb23c1544dca527bb5d1199e8e60c3a3e
Thomas Legrand <thomas@ks22750.kimsufi.com>
2012-12-03 10:59:39 +0100

    commit by genmymodel from www.genmymodel.com: v20121203_105939

---
+++ src/com/genmymodel/petshop/domain/Address.java
@@ -0,0 +1,117 @@
+package com.genmymodel.petshop.domain;
+
+
+
+
+public class Address
+{
+
+	
+	
+	private String city;
+	
+	
+	private String country;
+	
+	
+	private Long id;
+	
+	
+	private String line1;
+	
+	
+	private String line2;
+    
+
+	
+	public String getCity () 
+	{
+			
+	}
+
+	
+	public String getCountry () 
+	{
+			
+	}
+
+	
+	public Long getId () 
+	{
+			
+	}
+
+	
+	public String getLine1 () 
+	{
+			
+	}
+
+	
+	public String getLine2 () 
+	{
+			
+	}
+
+	
+	public void setCity (String myCity) 
+	{
+		this.city = myCity;	
+	}
+
+	
+	public void setCountry (String myCountry) 
+	{
+		this.country = myCountry;	
+	}
+
+	
+	public void setId (Long myId) 
+	{
+		this.id = myId;	
+	}
+
+	
+	public void setLine1 (String myLine1) 
+	{
+		this.line1 = myLine1;	
+	}
+
+	
+	public void setLine2 (String myLine2) 
+	{
+		this.line2 = myLine2;	
+	}
+
+	
+	public void unsetCity () 
+	{
+		this.city = null;	
+	}
+
+	
+	public void unsetCountry () 
+	{
+		this.country = null;	
+	}
+
+	
+	public void unsetId () 
+	{
+		this.id = null;	
+	}
+
+	
+	public void unsetLine1 () 
+	{
+		this.line1 = null;	
+	}
+
+	
+	public void unsetLine2 () 
+	{
+		this.line2 = null;	
+	}
+
+
+}
