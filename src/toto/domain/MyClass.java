f4248d3a95adf1d39094c0018bb428b107fbf350
genmymodel <help@genmymodel.com>
2013-01-25 14:56:48 +0100

    commit by genmymodel from www.genmymodel.com: v20130125_145646

---
+++ src/toto/domain/MyClass.java
@@ -0,0 +1,31 @@
+package toto.domain;
+
+//Start of user code Import bloc for Class MyClass
+
+//End of user code
+
+
+
+public class MyClass
+{
+
+	//Start of user code Attribute bloc for Class MyClass
+
+	//End of user code
+
+	public MyClass(){
+		//Start of user code standard constructor bloc for Class MyClass
+
+		//End of user code
+	}
+
+	//Start of user code Other constructor bloc for Class MyClass
+
+	//End of user code
+
+	//Start of user code Method bloc for Class MyClass
+
+	//End of user code
+
+}
+
