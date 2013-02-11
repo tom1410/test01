856ec0ec7558d2ae698cde5a94dc8b1852393fad
genmymodel <help@genmymodel.com>
2013-02-11 10:13:07 +0100

    commit by genmymodel from www.genmymodel.com: v20130211_101305

---
+++ src/main/java/toto/MyClass.java
@@ -0,0 +1,23 @@
+package toto;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class MyClass
+{
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public MyClass(){
+		
+	}
+
+}
+
