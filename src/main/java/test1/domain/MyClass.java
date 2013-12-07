3cab9e33adc476af1987aa5b1771636d38a43a0c
genmymodel <help@genmymodel.com>
2013-12-07 13:00:21 +0100

    commit by genmymodel from www.genmymodel.com: v20131207_130020

--- src/main/java/test1/domain/MyClass.java
+++ src/main/java/test1/domain/MyClass.java
@@ -1,149 +1,20 @@
 package test1.domain;
+
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
 import test1.domain.mypackage.MyClass2;
 
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(sequenceName = "MYCLASS_SEQ")
+public class MyClass {
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
- 
-@javax.persistence.Entity 
-public class MyClass
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.Column(nullable = false) 
-	protected byte[] attribute;
+    @NotNull
+    private Byte Myattribute;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.OneToOne(mappedBy = "myClass") 
-	protected MyClass2 myClass2;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	@javax.persistence.Id 
-	@javax.persistence.Column(nullable = false) 
-	protected final Long id = 0L;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public MyClass(){
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void basicSetMyClass2(MyClass2 myMyClass2) {
-		if (this.myClass2 != myMyClass2) {
-			if (myMyClass2 != null){
-				if (this.myClass2 != myMyClass2) {
-					MyClass2 oldmyClass2 = this.myClass2;
-					this.myClass2 = myMyClass2;
-					if (oldmyClass2 != null)
-						oldmyClass2.unsetMyClass();
-				}
-			}
-		}	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public byte[] getAttribute() {
-		return this.attribute;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public MyClass2 getMyClass2() {
-		return this.myClass2;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public long getId() {
-		return this.id;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setAttribute(byte[] myAttribute) {
-		this.attribute = myAttribute;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setMyClass2(MyClass2 myMyClass2) {
-		this.basicSetMyClass2(myMyClass2);
-		myMyClass2.basicSetMyClass(this);
-			
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetAttribute() {
-		this.attribute = new byte[32];	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetMyClass2() {
-		if (this.myClass2 == null)
-			return;
-		MyClass2 oldmyClass2 = this.myClass2;
-		this.myClass2 = null;
-		oldmyClass2.unsetMyClass();	
-	}
-	
+    @OneToOne
+    private MyClass2 myClass2;
 }
-
--- src/main/java/test1/domain/mypackage/MyClass2.java
+++ src/main/java/test1/domain/mypackage/MyClass2.java
@@ -1,229 +1,25 @@
 package test1.domain.mypackage;
-import test1.domain.MyClass;
 
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
- 
-@javax.persistence.Entity 
-public class MyClass2
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.Column(nullable = false) 
-	protected String attribute;
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(sequenceName = "MYCLASS2_SEQ")
+public class MyClass2 {
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.Column(nullable = false) 
-	protected long attribute2;
+    @NotNull
+    private MyClass attribute3;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.OneToOne 
-	protected MyClass attribute3;
+    @NotNull
+    private long attribute2;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	 
-	@javax.persistence.OneToOne 
-	protected MyClass myClass;
+    @NotNull
+    private String Myattribute;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	@javax.persistence.Id 
-	@javax.persistence.Column(nullable = false) 
-	protected final Long id = 0L;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public MyClass2(){
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void basicSetMyClass(MyClass myMyClass) {
-		if (this.myClass != myMyClass) {
-			if (myMyClass != null){
-				if (this.myClass != myMyClass) {
-					MyClass oldmyClass = this.myClass;
-					this.myClass = myMyClass;
-					if (oldmyClass != null)
-						oldmyClass.unsetMyClass2();
-				}
-			}
-		}	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public String getAttribute() {
-		return this.attribute;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public long getAttribute2() {
-		return this.attribute2;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public MyClass getAttribute3() {
-		return this.attribute3;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public MyClass getMyClass() {
-		return this.myClass;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public long getId() {
-		return this.id;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setAttribute(String myAttribute) {
-		this.attribute = myAttribute;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setAttribute2(long myAttribute2) {
-		this.attribute2 = myAttribute2;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setAttribute3(MyClass myAttribute3) {
-		this.attribute3 = myAttribute3;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void setMyClass(MyClass myMyClass) {
-		this.basicSetMyClass(myMyClass);
-		myMyClass.basicSetMyClass2(this);
-			
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetAttribute() {
-		this.attribute = "";	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetAttribute2() {
-		this.attribute2 = 0L;	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetAttribute3() {
-		this.attribute3 = new MyClass();	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	public void unsetMyClass() {
-		if (this.myClass == null)
-			return;
-		MyClass oldmyClass = this.myClass;
-		this.myClass = null;
-		oldmyClass.unsetMyClass2();	
-	}
-	
+    @OneToOne
+    private test1.domain.MyClass myClass;
 }
-
